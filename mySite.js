e072fec2cd7974222d2b37a0d264f7eddbb44c3b
Demonshae Glover <dshaeglover@gmail.com>
2017-07-15 10:33:49 -0400

    This is my site

---
+++ mySite.js
@@ -0,0 +1,13 @@
+//execute this when DOM is ready
+$(function(){
+  //what to do when li is clicked
+$('li').click(function(){
+//$(this).css('background-color', 'red').html('Clicked' );
+$(this).toggleClass('red-background');
+//this is a keyword that referenced a specific object
+});
+
+});
+
+
+//for steps 4 and 5 there is a click method to finish the assignment
